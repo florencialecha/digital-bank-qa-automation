4f23f1f72bca85ffd46da489dd08d499c1359c75
Florencia Lecha <flecharegueira@gmail.com>
2023-05-15 11:59:44 -0300

    First commit

---
+++ qa-automation/src/main/java/com/digitalbank/qaautomation/QaAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.digitalbank.qaautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaAutomationApplication.class, args);
+	}
+
+}
---
+++ qa-automation/src/test/java/com/digitalbank/qaautomation/QaAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalbank.qaautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
