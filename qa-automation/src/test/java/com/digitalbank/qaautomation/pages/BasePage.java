2ab0b489f15c1a87ae84f94fb78995c142cf4e5f
Florencia Lecha <flecharegueira@gmail.com>
2023-05-15 12:13:31 -0300

    Add BasePage

---
+++ qa-automation/src/test/java/com/digitalbank/qaautomation/pages/BasePage.java
@@ -0,0 +1,15 @@
+package com.digitalbank.qaautomation.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+
+    public static WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        BasePage.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+}
