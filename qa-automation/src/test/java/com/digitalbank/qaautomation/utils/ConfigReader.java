3492fb4af7b032ac2fc770749cb67a4e16da474d
Florencia Lecha <flecharegueira@gmail.com>
2023-05-24 17:35:05 -0300

    Add ConfigReader route and implement on AccountExistViewTest

---
+++ qa-automation/src/test/java/com/digitalbank/qaautomation/utils/ConfigReader.java
@@ -0,0 +1,28 @@
+package com.digitalbank.qaautomation.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private Properties properties;
+
+    public ConfigReader() {
+        properties = new Properties();
+        try {
+            FileInputStream inputStream = new FileInputStream("src/test/java/com/digitalbank/qaautomation/resources/config.properties");
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getLoginUrl() {
+        return properties.getProperty("app.loginUrl");
+    }
+
+    public String getHomeUrl() {
+        return properties.getProperty("app.homeUrl");
+    }
+}
